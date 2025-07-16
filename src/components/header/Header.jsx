7d851bb8c20fe43b9d06598bb7f1071207e70715
rahim4shanewaz <soumiq444@gmail.com>
2025-07-17 04:49:08 +0600

    project setup using vite, tailwindcss

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className='bg-slate-100 p-5'>
+            <h1 className='text-center font-bold text-3xl'>Header</h1>
+        </div>
+    );
+};
+
+export default Header;
